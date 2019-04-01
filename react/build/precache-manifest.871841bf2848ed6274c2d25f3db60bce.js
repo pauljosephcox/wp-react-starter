3489549ef8172395368a9e03b4d4461c05950b2c
Paul Cox <pauljosephcox@gmail.com>
2019-03-31 22:07:03 -0400

    Include Shortcode example

---
+++ react/build/precache-manifest.871841bf2848ed6274c2d25f3db60bce.js
@@ -0,0 +1,10 @@
+self.__precacheManifest = [
+  {
+    "revision": "7583396598956f03c937",
+    "url": "/static/js/main.js"
+  },
+  {
+    "revision": "2ecff00d0f3e783058d47927743a5568",
+    "url": "/index.html"
+  }
+];
