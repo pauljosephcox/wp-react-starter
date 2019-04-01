a186d80695ba7e951760f3e3ffc4e9402cd987b3
Paul Cox <pauljosephcox@gmail.com>
2019-03-31 22:01:24 -0400

    example usage

--- templates/church-map.php
+++
@@ -1,5 +0,0 @@
-</div><!-- wsyiwyg -->
-<div class="groups-engine">
-    <TCCGroups site="<?php echo site_url(); ?>"></TCCGroups>
-</div>
-<div class="wysiwyg">
---
+++ templates/component-hello-world.php
@@ -0,0 +1,3 @@
+<div class="example-shortcode">
+    <HelloWorld name="<?php echo $attr['name']; ?>"></HelloWorld>
+</div>
